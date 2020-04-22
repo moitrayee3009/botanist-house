7bac0aa0e9191e2c636c64f76614def69d045247
Moitrayee Choudhury <moitrayee3009@gmail.com>
2020-04-22 18:44:11 +0200

    added new functionality to catch Fimg_url

--- functions.php
+++ functions.php
@@ -66,3 +66,27 @@ function storefront_remove_storefront_breadcrumbs() {
 add_action( 'init', function() {
     remove_action( 'storefront_loop_post', 'storefront_post_header', 10 );
 } );
+
+/**
+ * Featured image
+*/
+
+
+add_action('rest_api_init', 'register_rest_images' );
+function register_rest_images(){
+    register_rest_field( array('post'),
+        'fimg_url',
+        array(
+            'get_callback'    => 'get_rest_featured_image',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+}
+function get_rest_featured_image( $object, $field_name, $request ) {
+    if( $object['featured_media'] ){
+        $img = wp_get_attachment_image_src( $object['featured_media'], 'app-thumb' );
+        return $img[0];
+    }
+    return false;
+}
