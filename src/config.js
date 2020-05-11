307b4cbac9a6f409003c7a9d6f65452f48d36fca
barntarnst <par@accomplice.se>
2020-05-11 11:13:28 +0200

    Added config file

---
+++ src/config.js
@@ -0,0 +1,9 @@
+//
+
+export default {
+  staging: {
+    URL: 'https://botanist-staging.accomplice.se/',
+    VUE_APP_CONSUMER_KEY: 'ck_9d4cd53b43ad93fd6d84953813c123d6099efc80',
+    VUE_APP_CONSUMER_SECRET: 'cs_f8abacb3cfe16cb6b2b6b6c387b45e1d192202e2'
+  }
+}
